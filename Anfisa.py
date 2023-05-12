5deba8887622eee1e25a2606f9ba75b15b1d3ccd
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-12 20:45:51 +0200

    up messages_count

--- Anfisa.py
+++ Anfisa.py
@@ -39,8 +39,8 @@ for messages_count in range(0, 21):
         print('У вас нет новых сообщений')
     elif messages_count == 1:
         print('У вас 1 новое сообщение')
-    elif messages_count < 5:
-        print('У вас', messages_count, 'новых сообщения')
+    elif messages_count >=2 and messages_count <= 4:
+        print(f'У вас {messages_count} новых сообщения')
     else:
         print(f'У вас {messages_count} новых сообщений')
 
