446d9584b4492480cbb0f405b65fa9f17d0bd3fd
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-12 20:44:18 +0200

    rework time_hello

--- Anfisa.py
+++ Anfisa.py
@@ -45,8 +45,15 @@ for messages_count in range(0, 21):
         print(f'У вас {messages_count} новых сообщений')
 
 
-for current_hour in range(24):
-    if current_hour < 12:
+for current_hour in range(0, 24):
+    print("На часах " + str(current_hour) + ":00.")
+    if current_hour < 6:
+        print('Доброй ночи!') 
+    elif current_hour < 12:
         print('Доброе утро!')
-    if current_hour >= 12:
+    elif current_hour < 18:
         print('Добрый день!')
+    elif current_hour < 23:
+        print('Добрый вечер!')
+    else:
+        print('Доброй ночи!')
