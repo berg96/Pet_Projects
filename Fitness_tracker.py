937b9cef78561e4d30a7bb0d2300e77c26f755cf
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-19 17:16:40 +0200

    great output

--- Fitness_tracker.py
+++ Fitness_tracker.py
@@ -3,22 +3,26 @@ height = 175 # Рост
 steps = 8462 # Количество пройденных за день шагов
 hours = 2 # Время движения в часах
 len_step_m = 0.65 # Длина одного шага в метрах
-transfer_coeff = 1000 # Коэффициент перевода значения расстояния из метров в километры
+transfer_coeff = 1000 # Коэффициент перевода значения растояния из метров в километры
 
-dist = steps * len_step_m / transfer_coeff # Напишите формулу расчёта
+dist = steps*len_step_m / transfer_coeff
 
 mean_speed = dist / hours
 minutes = hours * 60
 
+if dist >= 6.5:
+    achievement = 'Отличный результат! Цель достигнута.'
+elif dist >= 3.9:
+    achievement = 'Неплохо! День был продуктивным.'
+elif dist >= 2:
+    achievement = 'Маловато, но завтра наверстаем!'
+else:
+    achievement = 'Лежать тоже полезно. Главное — участие, а не победа!'
+
 spent_calories = (0.035*weight + (mean_speed**2 / height) * 0.029*weight) * minutes
 
-output = f'Сегодня вы прошли {dist:.2f} км и затратили {spent_calories:.2f} килокалорий.'  # Здесь подготовьте строку для вывода
-if dist >= 6.5:
-    output += ' Отличный результат! Цель достигнута.'
-elif dist >= 3.9:
-    output += ' Неплохо! День был продуктивным.'
-elif dist>= 2:
-    output += ' Маловато, но завтра наверстаем!'
-else:
-    output += ' Лежать тоже полезно. Главное — участие, а не победа!'
+output = f'''Сегодня вы прошли {steps} шагов.
+Пройденная дистанция {dist:.2f} км.
+Вы сожгли {spent_calories:.2f} ккал.
+{achievement}'''         
 print(output)
