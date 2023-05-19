ea063dacbf3d6351af00b3d0e2a45884d542fd42
Tema IceBerg Kulikov <chigar2010@yandex.ru>
2023-05-19 17:04:39 +0200

    calc dist and output this

---
+++ Fitness_tracker.py
@@ -0,0 +1,16 @@
+weight = 75 # Вес
+height = 175 # Рост
+steps = 8462 # Количество пройденных за день шагов
+hours = 2 # Время движения в часах
+len_step_m = 0.65 # Длина одного шага в метрах
+transfer_coeff = 1000 # Коэффициент перевода значения расстояния из метров в километры
+
+dist = steps * len_step_m / transfer_coeff # Напишите формулу расчёта
+
+mean_speed = dist / hours
+minutes = hours * 60
+
+spent_calories = (0.035*weight + (mean_speed**2 / height) * 0.029*weight) * minutes
+
+output = f'{dist} {spent_calories}'  # Здесь подготовьте строку для вывода
+print(output)
